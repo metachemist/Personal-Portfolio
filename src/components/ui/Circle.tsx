3e6dc8ee1665c754cc8cf195b1adfdd42d9594e4
metachemist <meta.chemist.22@gmail.com>
2024-11-11 06:42:55 +0500

    Changes in circle

--- src/components/ui/Circle.tsx
+++ src/components/ui/Circle.tsx
@@ -1,4 +1,11 @@
-import { Poppins } from "next/font/google";
+import { Playfair_Display, Poppins } from "next/font/google";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const playfair = Playfair_Display({
+  weight: ["400", "700", "900"], // Specify the weights you need
+  style: ["normal", "italic"], // Define styles if needed
+  subsets: ["latin"],
+});
 
 const poppins = Poppins({
     weight: ["400", "700"], // Specify the weights you need
@@ -9,8 +16,9 @@ const poppins = Poppins({
   
 const Circle = () => {
   return (
-    <div className={`absolute -bottom-30 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-28 h-28 bg-baseOrange rounded-full flex items-center justify-center z-10`}>
-      <span className={`text-baseBlack font-semibold text-md ${poppins.className}`}>Scroll Down</span>
+    <div className={`absolute -bottom-30 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[120px] h-[120px] bg-baseOrange rounded-full flex items-center justify-center z-10`}>
+      <span className={`text-baseBlack font-semibold text-md ${poppins.className}`}>Scroll down</span>
+      
     </div>
    
   );
